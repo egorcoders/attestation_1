7a98e5a82cd62dc3b473f378cee2653fb093aebd
Egor Boev <egorcoders@gmail.com>
2022-10-03 15:50:10 +0500

    #3 v.6

--- emergency/emergency_request/delete.py
+++ emergency/emergency_request/delete.py
@@ -1,11 +1,2 @@
-from django.db import models
-
-
-class Person(models.Model):
-    SHIRT_SIZES = {
-        ('S', 'Small'),
-        ('M', 'Middle'),
-        ('L', 'Large'),
-    }
-    first_name = models.CharField(max_length=50)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+n = str(input())
+print('Привет, {}'.format(n))
